8406452fd2f6bbea59056ef6de8392d6d9546969
Derek-garza <a.22308051281196@cbtis128.edu.mx>
2025-02-11 16:53:50 +0000

    hola lista

---
+++ lista1.dart
@@ -0,0 +1,13 @@
+void main() {  
+  // lista de tipo entero 
+   List <int> list1 = [210,21,22,];  
+   print("Lista de enteros:");
+   print(list1);  
+   print("primer elemento: ${list1[0]}");
+  print("segundo elemento: ${list1[1]}");
+    print("tercer elemento: ${list1[2]}");
+    print("elementos de la lista usando ciclo for : ${list1.length}");
+    for (int i = 0; i < list1.length; i++) {
+    print(list1[i]);
+}  
+}
