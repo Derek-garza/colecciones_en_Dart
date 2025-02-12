4c1ddb05df81ef0ff8a1ff63b7e19f94b7f53898
Derek-garza <a.22308051281196@cbtis128.edu.mx>
2025-02-12 20:34:39 +0000

    mapa_tabla_Cliente_Empleado

---
+++ mapa2.dart
@@ -0,0 +1,44 @@
+
+void main() {
+ 
+  Map<String, dynamic> Cliente = {
+    'id_cliente': '111',
+    'nombre': 'Juan',
+    'fecha_nac': '1990-01-01',
+    'correo': 'juan@gmail.com',
+    'direccion': 'calle pene',
+    'telefono': '656 777 333',
+    'codigo p': '1221'
+    };
+  print("Derek Santiago Garza Martinez 22308051281196 gpo-6-J");  
+  print("Datos del cliente");
+  print(Cliente['id_cliente']);
+  print(Cliente['nombre']);
+  print(Cliente['fecha_nac']);
+  print(Cliente['correo']);
+  print(Cliente['direccion']);
+  print(Cliente['telefono']);
+  print(Cliente['codigo p']);
+  
+   Map<String, dynamic> empleados = {
+    'id_empleado': '111',
+    'curp': 'Juan',
+    'telefono': '1990-01-01',
+    'direccion': 'juan@gmail.com',
+    'nombre': 'calle pene',
+    'fecha_nac': '656 777 333',
+    'sexo': '1221',
+    'id_sucursal': '1221'
+    };
+
+  print("Datos del empleado");
+  print(empleados['id_empleado']);
+  print(empleados['curp']);
+  print(empleados['telefono']);
+  print(empleados['direccion']);
+  print(empleados['nombre']);
+  print(empleados['fecha_nac']);
+  print(empleados['sexo']); 
+  print(empleados['id_sucursal']);
+
+  }
---
+++ mapa2.md
@@ -0,0 +1,4 @@
+salida de los datos del mapa Cliente 
+![alt text](image-3.png)
+Salida de los datos mapa Empleados
+![alt text](image-4.png)
