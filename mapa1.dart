55eb03fb334013ce3d22dc7ce97a32a900b2c971
Derek-garza <a.22308051281196@cbtis128.edu.mx>
2025-02-12 20:07:39 +0000

    ejemplo mapa

---
+++ mapa1.dart
@@ -0,0 +1,25 @@
+
+void main () 
+{
+print("Derek Santiago Garza Martinez 22308051281196 gpo-6-J");
+
+Map<int,String> alumnos= {
+
+1: "Derek",
+2: "Santiago",
+3: "Garza"};
+
+print("Mapa de alumnos:");
+print(alumnos);
+
+print ("iterar un map con forEach");
+alumnos.forEach((key,value)
+{
+  print("$key, $value");
+});
+print("iterar un map con for in");
+for (var value in alumnos.values)
+{
+  print("$value");
+}
+}
